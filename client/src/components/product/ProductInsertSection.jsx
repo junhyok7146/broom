425484e8b6265b2e53f7b93044573a9887a0a80e
audtnnn <kaudtn12@gmail.com>
2024-06-07 15:53:13 +0900

    11

--- client/src/components/product/ProductInsertSection.jsx
+++ client/src/components/product/ProductInsertSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import axios from 'axios';
 import Slider from "react-slick";
@@ -51,7 +51,6 @@ const ProductInsertSectionBlock = styled.div`
         }
         .time {
             select {
-                flex:1;
                 background: #0059e9;
                 color: #fff;
                 border-radius: 4px;
@@ -73,9 +72,6 @@ const ProductInsertSectionBlock = styled.div`
                 background: #fafafa;
                 border-radius: 4px;
                 padding: 0 30px;
-                &:focus{
-                    outline: 2px solid #0059e9;
-                }
             }
         }
         .address {
@@ -91,8 +87,9 @@ const ProductInsertSectionBlock = styled.div`
                     background: #fafafa;
                     border-radius: 4px;
                     padding: 0 30px;
+                    height: 50px;
                     &:focus{
-                        outline: 2px solid #0059e9;
+                        outline: 1px solid #0059e9;
                     }
                 }
                 button {
@@ -107,52 +104,10 @@ const ProductInsertSectionBlock = styled.div`
                 flex-direction: column;
                 gap: 10px;
                 input {
-                    width: 100%;
-                    height: 50px;
-                    background: #fafafa;
-                    border-radius: 4px;
-                    padding: 0 30px;
                     &:focus{
-                        outline: 2px solid #0059e9;
+                        outline: 1px solid #0059e9;
                     }
                 }
-            }
-            .addr2 {
-                width: 100%;
-                input {
-                    width: 100%;
-                    height: 50px;
-                    background: #fafafa;
-                    border-radius: 4px;
-                    padding: 0 30px;
-                    &:focus{
-                        outline: 2px solid #0059e9;
-                    }
-                }
-            }
-        }
-        .homeType {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 30px;
-            select {
-                width: 100%;
-                height: 50px;
-                background: #0059e9;
-                color: #fff;
-                border: none;
-                border-radius: 4px;
-                appearance: none;
-                -moz-appearance: none;
-                -webkit-appearance: none;
-                outline: none;
-                cursor: pointer;
-                text-align: center;
-            }
-            option{
-                background: #fff;
-                color: #000;
             }
         }
         .serviceType {
@@ -169,31 +124,21 @@ const ProductInsertSectionBlock = styled.div`
                 color: #fff;
             }
         }
-        .description {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 30px;
-
-            textarea {
-                width: 500px;
-                height: 200px;
-                border-radius: 5px;
-                border: 1px solid #0059e9;
-                padding: 20px;
+        input, select, textarea, button {
+            width: 100%;
+            max-width: 500px;
+            height: 39px;
+        }
+        .btn {
+            button {
+                background: #0059e9;
+                color: #fff;
+                padding: 10px 30px;
+                border-radius: 4px;
+                cursor: pointer;
                 &:focus {
                     outline: none;
                 }
-            }
-        }
-        .btn {
-            display: flex;
-            justify-content: center;
-            button{
-                background:#0059e9;
-                padding: 10px 30px;
-                color: #fff;
-                border-radius: 4px;
             }
         }
     }
@@ -217,11 +162,21 @@ const ProductInsertSection = () => {
     const [totalPrice, setTotalPrice] = useState(0);
     const mAddressSubRef = useRef("");
     const [photoValue, setPhotoValue] = useState("");
+    const sliderRef = useRef(null);
+    const [isTyping, setIsTyping] = useState(false);
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+    };
+
     const handleChange = (e) => {
         const { value, name } = e.target;
-        if(name== 'homeType'){
+        if(name === 'homeType'){
             let homeTypePrice = 0;
-        if (name === 'homeType') {
             if (value === '다세대·다가구') {
                 homeTypePrice = 150000;
             } else if (value === '오피스텔·도시형생활주택') {
@@ -229,11 +184,20 @@ const ProductInsertSection = () => {
             } else if (value === '아파트') {
                 homeTypePrice = 300000;
             }
-        }
-        setTotalPrice(homeTypePrice)
+            setTotalPrice(homeTypePrice);
         }
         setProduct(product => ({ ...product, [name]: value }));
     };
+
+    const handleInputFocus = () => {
+        setIsTyping(true); // 입력 중으로 설정
+    };
+
+    const handleInputBlur = () => {
+        setIsTyping(false); // 입력이 끝남
+        sliderRef.current.slickNext();
+    };
+
     const handleFileChange = (e) => {
         const file = e.target.files[0];
         setProduct(prevProduct => ({ ...prevProduct, photo: file }));
@@ -258,13 +222,11 @@ const ProductInsertSection = () => {
         }
         formData.append("userNo", user.userNo);
 
-        axios.post("http://localhost:8001/product/register", formData, {
-            headers: {
-                "Content-Type": "multipart/form-data",
-            }
-        })
-        .then((res) => {
-            if (res.data.affectedRows == 1) {
+        try {
+            const res = await axios.post("http://localhost:8001/product/register", formData, {
+                headers: { "Content-Type": "multipart/form-data" },
+            });
+            if (res.data.affectedRows === 1) {
                 setProduct({
                     category: "no_select",
                     name: "",
@@ -278,7 +240,7 @@ const ProductInsertSection = () => {
                     price: 0,
                     photo: ""
                 });
-                setPhotoValue();
+                setPhotoValue("");
             } else {
                 alert("상품등록 실패");
             }
@@ -334,60 +296,99 @@ const ProductInsertSection = () => {
                 <img src="/src/assets/image/logo_color.png" alt="" />
             </div>
             <form onSubmit={onSubmit}>
-                <div className='time'>
-                    <select name="category" id="category" value={product.category} onChange={handleChange}>
-                        <option value="no_select" disabled>방문 희망시간</option>
-                        <option value="오전(8시~10시)">오전(8시~10시)</option>
-                        <option value="오후(13시~15시)">오후(13시~15시)</option>
-                        <option value="저녁(15시~17시)">저녁(15시~17시)</option>
-                        <option value="언제든 가능">언제든 가능</option>
-                    </select>
-                </div>
-                <div className='getName'>
-                    <label htmlFor="name">성함을 입력해주세요.</label>
-                    <input required type="text" name="name" id="name" value={product.name} onChange={handleChange} placeholder='이름' />
-                </div>
-                <div className='address'>
-                    <label htmlFor="addr1">부름을 부르실 주소를 입력해주세요.</label>
-                    <div className='postNo'>
-                        <input style={{ width:'150px'}} type="text" name="zipCode" id="zipCode" value={product.zipCode} onChange={handleChange} readOnly placeholder='우편번호'  />
-                        <button type="button" onClick={openDaumPostcode} style={{ height:'50px', verticalAlign:'middle', padding:'0 5px', marginRight:'5px'}}>주소검색</button>
+                <Slider ref={sliderRef} {...settings}>
+                    <div className='time'>
+                        <div className='container'>
+                            <div className='min_title'>
+                                <label htmlFor="category">방문하고자 하는 시간을<br/> 선택해주세요.</label>
+                            </div>
+                            <div className='min_content'>
+                                <select name="category" id="category" value={product.category} onChange={handleChange}  onFocus={handleInputFocus} onBlur={handleInputBlur}>
+                                    <option value="no_select" disabled>방문 희망시간</option>
+                                    <option value="오전(8시~10시)">오전(8시~10시)</option>
+                                    <option value="오후(13시~15시)">오후(13시~15시)</option>
+                                    <option value="저녁(15시~17시)">저녁(15시~17시)</option>
+                                    <option value="언제든 가능">언제든 가능</option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
-                    <div className='addr1'>
-                        <input type="text" name="addr1" id="addr1" value={product.addr1} onChange={handleChange} onClick={openDaumPostcode} readOnly placeholder='주소입력' />
+                    <div className='name'>
+                        <div className='container'>
+                            <div className='min_title'>
+                                <label htmlFor="name">성함을 입력해주세요.</label>
+                            </div>
+                            <div className='min_content'>
+                                <input required type="text" name="name" id="name" value={product.name} onChange={handleChange} onFocus={handleInputFocus} onBlur={handleInputBlur} placeholder='이름' />
+                            </div>
+                        </div>
                     </div>
-                    <div className='addr2'>
-                        <input type="text" name="addr2" id="addr2" ref={mAddressSubRef} value={product.addr2} onChange={handleChange} placeholder='층,동,호수 입력' />
+                    <div className='address'>
+                        <div className='container'>
+                            <div className='min_title'>
+                                <label htmlFor="addr1">부름을 부르실 주소를<br/> 입력해주세요.</label>
+                            </div>
+                            <div className='min_content'>
+                                <div className='postNo'>
+                                    <input style={{ width: '150px' }} type="text" name="zipCode" id="zipCode" value={product.zipCode} onChange={handleChange} readOnly placeholder='우편번호' />
+                                    <button type="button" onClick={openDaumPostcode} style={{ height: '50px', verticalAlign: 'middle', padding: '0 5px', marginRight: '5px' }}>주소검색</button>
+                                </div>
+                                <div className='addr1'>
+                                    <input type="text" name="addr1" id="addr1" value={product.addr1} onChange={handleChange} onClick={openDaumPostcode} readOnly placeholder='주소입력' />
+                                </div>
+                                <div className='addr2'>
+                                    <input type="text" name="addr2" id="addr2" ref={mAddressSubRef} value={product.addr2}  onFocus={handleInputFocus} onBlur={handleInputBlur} onChange={handleChange} placeholder='층,동,호수 입력' />
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className='homeType'>
-                    <label htmlFor="homeType">건물의 유형을 선택해주세요.</label>
-                    <select name="homeType" id="homeType" value={product.homeType} onChange={handleChange}>
-                        <option value="no_select" disabled>선택</option>
-                        <option value="다세대&#183;다가구">다세대&#183;다가구</option>
-                        <option value="오피스텔&#183;도시형생활주택">오피스텔&#183;도시형생활주택</option>
-                        <option value="아파트">아파트</option>
-                    </select>
-                </div>
-                <div className='serviceType'>
-                    <label>서비스 유형을 선택해주세요.</label>
-                    <button type="button" className={product.productType === "기본형" ? "selected" : ""} onClick={() => handleProductTypeClick("기본형")}>기본형</button>
-                    <button type="button" className={product.productType === "고급형" ? "selected" : ""} onClick={() => handleProductTypeClick("고급형")}>고급형</button>
-                </div>
-                <div className='description'>
-                    <label htmlFor="description">특이사항이 있으면 남겨주세요</label>
-                    <textarea name="description" id="description" value={product.description} onChange={handleChange}></textarea>
-                </div>
-                <div>
-                    <label htmlFor="photo">상품사진:</label>
-                    <input type="file" name="photo" id="photo" value={photoValue} onChange={handleFileChange} />
-                </div>
-                <div className="btn">
-                    <button type="submit">등록</button>
-                </div>
-                <div>
-                    예상금액 {totalPrice}
-                </div>
+                    <div className='homeType'>
+                        <div className='container'>
+                            <div className='min_title'>
+                                <label htmlFor="homeType">건물의 유형을 선택해주세요.</label>
+                            </div>
+                            <div className='min_content'>
+                                <select name="homeType" id="homeType" value={product.homeType} onChange={handleChange}  onFocus={handleInputFocus} onBlur={handleInputBlur}>
+                                    <option value="no_select" disabled>선택</option>
+                                    <option value="다세대&#183;다가구">다세대&#183;다가구</option>
+                                    <option value="오피스텔&#183;도시형생활주택">오피스텔&#183;도시형생활주택</option>
+                                    <option value="아파트">아파트</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='serviceType'>
+                        <div className='container'>
+                            <div className='min_title'>
+                                <label>서비스 유형을 선택해주세요.</label>
+                            </div>
+                            <div className='min_content'>
+                                <button type="button" className={product.productType === "기본형" ? "selected" : ""} onClick={() => handleProductTypeClick("기본형")}>기본형</button>
+                                <button type="button" className={product.productType === "고급형" ? "selected" : ""} onClick={() => handleProductTypeClick("고급형")}>고급형</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='description'>
+                        <div className='container'>
+                            <div className='min_title'>
+                                <label htmlFor="description">특이사항이 있으면 남겨주세요</label>
+                            </div>
+                            <div className='min_content'>
+                                <textarea name="description" id="description" value={product.description} onChange={handleChange}></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="btn">
+                        <div className='container'>
+                            <div className='min_title'>
+                                <div style={{fontSize: '25px'}}>예상금액은 '{formatCurrency(totalPrice)}' 원 이에요. <br/> 접수를 등록하시겠어요?</div>
+                            </div>
+                            <div className='min_content'>
+                                <button type="submit">등록</button>
+                            </div>
+                        </div>
+                    </div>
+                </Slider>
             </form>
         </ProductInsertSectionBlock>
     );
